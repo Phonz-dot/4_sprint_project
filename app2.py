e4aa2728b5bd2eacbb58d8ef071d2ffa5467d955
Alphonso Houston <alhouston789@gmail.com>
2024-11-04 17:12:25 -0500

    updated app2

--- app2.py
+++ app2.py
@@ -8,7 +8,7 @@ st.header('Share of Odometer Readings')
 fig = px.histogram(vehicles_df, x='odometer')
 fig = fig.update_layout(width=800, height=400, margin=dict(l=50, r=50, t=50, b=50))
 
-#event = st.plotly_chart(fig, key="odometer", on_select="rerun")
+event = st.plotly_chart(fig, key="odometer", on_select="rerun")
 
 
 avg_price_per_year = vehicles_df.groupby('model_year')['price'].mean()
@@ -21,7 +21,6 @@ fig = px.scatter(avg_price_per_year, x='model_year', y='price', title='AVG Price
 
 event2 = st.plotly_chart(fig, key="AVG Price", on_select="rerun")
 
-event2
 
 toggle = st.checkbox("Toggle Between Graphs")
 

7a4f3f8cd1e2975f9fbc12836d57bf6db1ee9e49
Alphonso Houston <alhouston789@gmail.com>
2024-11-04 16:49:40 -0500

    updated app2

--- app2.py
+++ app2.py
@@ -5,16 +5,16 @@ vehicles_df = pd.read_csv('vehicles_us.csv')
 
 st.header('Share of Odometer Readings')
 
-df = px.vehicles_df()  # iris is a pandas DataFrame
-fig = px.histogram(vehicles_df, x='odometer')
+df = px.vehicles_df()
+fig = px.histogram(df, x='odometer')
 fig = fig.update_layout(width=800, height=400, margin=dict(l=50, r=50, t=50, b=50))
 
 event = st.plotly_chart(fig, key="odometer", on_select="rerun")
 
 event
 
-df = px.data.avg_price_per_year()  # iris is a pandas DataFrame
-fig = px.scatter(avg_price_per_year, x='model_year', y='price', title='AVG Price per Model Year')
+
+fig = px.scatter(df, x='model_year', y='price', title='AVG Price per Model Year')
 
 event2 = st.plotly_chart(fig, key="AVG Price", on_select="rerun")
 

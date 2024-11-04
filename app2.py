055a622b40015d1bf4f615e16c275421efb08041
Alphonso Houston <alhouston789@gmail.com>
2024-11-04 17:33:47 -0500

    updated app2

--- app2.py
+++ app2.py
@@ -4,11 +4,10 @@ import plotly.express as px
 vehicles_df = pd.read_csv('vehicles_us.csv')
 
 st.header('Share of Odometer Readings')
-
-fig = px.histogram(vehicles_df, x='odometer')
-fig = fig.update_layout(width=800, height=400, margin=dict(l=50, r=50, t=50, b=50))
-
-event = st.plotly_chart(fig, key="odometer", on_select="rerun")
+#First Plot
+fig1 = px.histogram(vehicles_df, x='odometer')
+fig1.update_layout(width=800, height=400, margin=dict(l=50, r=50, t=50, b=50))
+event1 = st.plotly_chart(fig1, key="odometer", on_select="rerun")
 
 
 avg_price_per_year = vehicles_df.groupby('model_year')['price'].mean()
@@ -16,17 +15,14 @@ avg_price_per_year = avg_price_per_year.reset_index()
 avg_price_per_year['model_year'] = avg_price_per_year['model_year'].astype(str)
 
 
-
-fig = px.scatter(avg_price_per_year, x='model_year', y='price', title='AVG Price per Model Year')
-
-event2 = st.plotly_chart(fig, key="AVG Price", on_select="rerun")
+#2nd Plot
+fig2 = px.scatter(avg_price_per_year, x='model_year', y='price', title='AVG Price per Model Year')
+event2 = st.plotly_chart(fig2, key="AVG Price", on_select="rerun")
 
 
 toggle = st.checkbox("Toggle Between Graphs")
-
-
 if toggle:
-      event
+    event1
 else:
-      event2
-   
+    event2
+      
